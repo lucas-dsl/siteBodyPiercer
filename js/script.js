efbe5d394158f41d3e6885424497a4f63293e913
Lucas <silvalucas71743@gmail.com>
2025-11-02 18:34:56 -0300

    ajustando nomes

--- js/script.js
+++ js/script.js
@@ -54,3 +54,41 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     });
 });
+
+// Animação de entrada de elementos
+const sr = ScrollReveal({
+    distance: "8rem",
+    duration: 1000,
+    delay: 200
+});
+
+// ANIMAÇÃO HERO
+sr.reveal('.logo-estudio', {origin: 'top'});
+sr.reveal('h1', {origin: 'right'});
+sr.reveal('.heroConteudo nav', {origin: 'bottom'});
+sr.reveal('.mouse', {origin: 'left'});
+
+// ANIMAÇÃO SOBRE
+sr.reveal('.sobre-esquerda', {origin: 'left'});
+sr.reveal('.sobre-direita', {origin: 'right'});
+
+// ANIMAÇÃO CATÁLOGO
+sr.reveal('section header', {origin: 'top'});
+sr.reveal('.caixaCatalogo:nth-of-type(1)', {origin: 'left'});
+sr.reveal('.caixaCatalogo:nth-of-type(2)', {origin: 'right'});
+
+// ANIMAÇÃO PORTFOLIO
+sr.reveal('.caixaPerfuracoes:nth-of-type(1)', {origin: 'left'});
+sr.reveal('.caixaPerfuracoes:nth-of-type(2)', {origin: 'right'});
+
+// ANIMAÇÃO LOCALIZAÇÃO E HORÁRIO
+sr.reveal('.caixa:nth-of-type(1)', {origin: 'left'});
+sr.reveal('.caixa:nth-of-type(2)', {origin: 'right'});
+sr.reveal('.mapa', {origin: 'bottom'});
+
+// FOOTER
+sr.reveal('footer', {origin: 'bottom'});
+
+
+
+
